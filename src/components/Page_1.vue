aebf08ba95ff67a65e8b5b504317534b635a09e5
saori <sieeya.bp@gmail.com>
2021-02-25 12:19:05 +0900

    componentsにファイルを追加

---
+++ src/components/Page_1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>ページ1</div>
+</template>
+
+<script>
+export default {
+name: "Page_1"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Page_2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title__main">ページ2</div>
+</template>
+
+<script>
+export default {
+name: "Page_2"
+}
+</script>
+
+<style lang="scss" scoped>
+.title__main {
+  font-size: 40px;
+  &:hover {
+   opacity: 0.2;
+  }
+}
+</style>
