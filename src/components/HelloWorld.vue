51a988ce7af385d7cb93c12b4c0ce70abb25394a
saori <sieeya.bp@gmail.com>
2021-03-07 23:30:59 +0900

    components/HelloWorldを変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+
     <p>{{ message }}</p>
     <p>{{ message_1.value }}</p>
     <p>{{ message_1.value.length }}</p>
@@ -7,13 +8,24 @@
     <p>{{ list[num] }}</p>
 
     <input type="text" v-bind:value="message">
+
+    <div v-bind:text-content.prop="message_2"></div>
+
+    <p>{{ count }}回クリックdone</p>
+    <button v-on:click="increment">カウントを増やす</button>
+
+    <p v-bind:class="{ child: idChild, 'is-active': isActive }">Text</p>
+    <p v-bind:style="{ color: textColor,backgroundColor: bgColor }">Text</p>
+
+    <p v-bind:class="classObject">Text</p>
+    <p v-bind:style="styleObject">Text</p>
+
   </div>
 </template>
 
 
 <script>
 export default {
-  //name: 'HelloWorld',
   data() {
     return {
       message: 'Hello World',
@@ -21,7 +33,26 @@ export default {
         value: 'Message1'
       },
       list: ['ぱんだ', 'くま', 'いるか'],
-      num: 1
+      num: 1,
+      message_2: 'Message2',
+      count: 0,
+      isChild: true,
+      isActive: true,
+      textColor: 'red',
+      bgColor: 'lightgray',
+      classObject: {
+        child: true,
+        'is-active': false
+      },
+      styleObject: {
+        color: 'blue',
+        backgroundColor: 'lightblue'
+      }
+    }
+  },
+  methods: {
+    increment: function () {
+      this.count += 1
     }
   }
 }
